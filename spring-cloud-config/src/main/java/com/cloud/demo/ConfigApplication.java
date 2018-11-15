5544feaec88c120acba2a9f70b5601be87adb364
lihaibozb <lihaibo19861126@126.com>
2018-11-15 20:01:40 +0800

    config modify

--- spring-cloud-config/src/main/java/com/cloud/demo/ConfigApplication.java
+++ spring-cloud-config/src/main/java/com/cloud/demo/ConfigApplication.java
@@ -2,10 +2,12 @@ package com.cloud.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigApplication {
 
 	public static void main(String[] args) {
