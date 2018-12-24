f4371d731405bd7d7cd1bf6e7bce3518d2e54e6e
lihaibozb <lihaibo19861126@126.com>
2018-12-24 16:20:18 +0800

    jwt

--- spring-cloud-oauth2/src/main/java/com/cloud/demo/config/AuthorizationServerConfig.java
+++ spring-cloud-oauth2/src/main/java/com/cloud/demo/config/AuthorizationServerConfig.java
@@ -3,11 +3,8 @@ package com.cloud.demo.config;
 import com.cloud.demo.security.DomainUserDetailsService;
 import com.cloud.demo.utils.PubUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -17,8 +14,6 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
-import org.springframework.util.StringUtils;
 
 /**
  * @author lihaibo
@@ -27,7 +22,6 @@ import org.springframework.util.StringUtils;
  * @Description: 授权服务器配置
  */
 @Configuration
-@RefreshScope
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
@@ -38,38 +32,21 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     private AuthenticationManager authenticationManager;
 
     /**
-     * redis连接工厂
-     */
-    @Autowired
-    private RedisConnectionFactory redisConnectionFactory;
-
-    @Value("${tokenType}")
-    private String tokenType;
-
-    /**
      * 令牌存储
      *
      * @return redis令牌存储对象
      */
     @Bean
     public TokenStore tokenStore() {
-        if (StringUtils.isEmpty(tokenType) || PubUtils.OAuth.TOKEN_TYPE_ACCESS.equals(tokenType)) {
-            return new RedisTokenStore(redisConnectionFactory);
-        } else {
             return new JwtTokenStore(accessTokenConverter());
-        }
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(this.authenticationManager)
                 .userDetailsService(new DomainUserDetailsService())
-                .tokenStore(tokenStore());
-
-        //如果为设置JWT token转换器
-        if (PubUtils.OAuth.TOKEN_TYPE_JWT.equals(tokenType)) {
-            endpoints.accessTokenConverter(accessTokenConverter());
-        }
+                .tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter());
     }
 
 
